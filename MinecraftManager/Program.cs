12291d7de91550c0f9d0ccbf205c31f1c57a960e
Ryan West <west.ryan.k@gmail.com>
2023-11-10 23:40:01 -0700

    Add countdown and add abort commands

--- MinecraftManager/Program.cs
+++ MinecraftManager/Program.cs
@@ -1,14 +1,19 @@
-﻿using System.Diagnostics;
+﻿using System.Data;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 
 namespace MinecraftManager;
 internal class Program
 {
-    private static readonly string _DefaultServerDirectory = Path.Combine("C:", "Program Files", "minecraft_servers", "lee_mindcrap_server");
+    private static readonly string _ServerDirectoryDefault = Path.Combine("C:", "Program Files", "minecraft_servers", "lee_mindcrap_server");
+    private static readonly TimeSpan _ShutdownCountdownDefault = TimeSpan.FromSeconds(10);
+
+    private static StreamWriter? _serverInput;
+
     async static Task Main(string[] args)
     {
-        Process serverProcess = StartMinecraftServer(_DefaultServerDirectory);
-        await ProcessServerOutputAsync(serverProcess);
+        Process serverProcess = StartMinecraftServer(_ServerDirectoryDefault);
+        await ProcessServerOutputAsync(serverProcess, _ShutdownCountdownDefault);
     }
 
     private static Process StartMinecraftServer(string serverDirectory)
@@ -26,17 +31,105 @@ internal class Program
             }
         };
         serverProcess.Start();
+        _serverInput = serverProcess.StandardInput;
         return serverProcess;
     }
 
-    private static async Task ProcessServerOutputAsync(Process serverProcess)
+    private static async Task ProcessServerOutputAsync(Process serverProcess, TimeSpan shutdownCountdown)
     {
+        CancellationTokenSource? shutdownCancel = null;
+
         while (!serverProcess.StandardOutput.EndOfStream)
         {
             string? input = await serverProcess.StandardOutput.ReadLineAsync();
+            Console.WriteLine(input);
             if (Line.TryParse(input, out Line line))
             {
+                if (line.Command.Equals("shutdown", StringComparison.OrdinalIgnoreCase))
+                {
+                    TryStartShutdownCountdown(shutdownCountdown, ref shutdownCancel);
+                }
+                else if (line.Command.Equals("cancel", StringComparison.OrdinalIgnoreCase))
+                {
+                    TryAbortShutdown(ref shutdownCancel);
+                }
             }
+        }
+    }
+
+    private static bool TryStartShutdownCountdown(TimeSpan shutdownCountdown, ref CancellationTokenSource? shutdownCancel)
+    {
+        try
+        {
+            if (shutdownCancel != null)
+            {
+                WriteMessage("Already shutting down");
+                return false;
+            }
+            shutdownCancel = new CancellationTokenSource();
+            CancellationToken token = shutdownCancel.Token;
+            Task.Run(() => CountdownShutdown(shutdownCountdown, token));
+        }
+        catch (Exception e)
+        {
+            WriteMessage(e.ToString());
+        }
+        return true;
+    }
+
+    private static async Task CountdownShutdown(TimeSpan shutdownCountdown, CancellationToken cancel)
+    {
+        WriteMessage($"Shutting down in {shutdownCountdown.TotalSeconds} seconds");
+        WriteMessage($"Type 'cancel' to abort shutdown");
+        await WriteCountdown(shutdownCountdown, cancel);
+        if (cancel.IsCancellationRequested)
+        {
+            return;
+        }
+        WriteMessage("Shut down started");
+    }
+
+    private static async Task WriteCountdown(TimeSpan totalTime, CancellationToken cancel) {
+        int seconds = (int)Math.Ceiling(totalTime.TotalSeconds);
+        for (int i = seconds; i > 0; i--)
+        {
+            WriteMessage($"{i}");
+            await Task.Delay(TimeSpan.FromSeconds(1), cancel);
+            if (cancel.IsCancellationRequested)
+            {
+                return;
+            }
+        }
+    }
+
+    private static bool TryAbortShutdown(ref CancellationTokenSource? shutdownCancel)
+    {
+        if (shutdownCancel == null)
+        {
+            WriteMessage("Nothing to abort");
+            return false;
+        }
+
+        WriteMessage("Aborting shutdown");
+        shutdownCancel.Cancel();
+        shutdownCancel = null;
+        return true;
+    }
+
+    private static void WriteMessage(string message)
+    {
+        WriteToServer($"/say {message}");
+    }
+
+    private static void WriteToServer(string command)
+    {
+        if (_serverInput != null)
+        {
+            _serverInput.WriteLine(command);
+        }
+        else
+        {
+            Console.WriteLine($"Error writing: '{command}'");
         }
     }
 }
